c9504647503006cf84b00b9d53389f05b668ee76
Nisha Pradeep <nishapradeep97@gmail.com>
2025-12-01 15:27:38 +0530

    Updated Exploratory Data Analysis.sql with new queries

--- SQL Tutorials/Exploratory Data Analysis.sql
+++ SQL Tutorials/Exploratory Data Analysis.sql
@@ -28,3 +28,45 @@ SELECT stage,SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY stage
 ORDER BY 2 DESC;
+
+SELECT company,AVG(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT SUBSTRING(`date`,1,7) AS monthh, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY monthh
+ORDER BY 1 ;
+
+WITH rolling_total AS
+(SELECT SUBSTRING(`date`,1,7) AS monthh, SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY monthh
+ORDER BY 1 
+)
+SELECT monthh,total_off,
+ SUM(total_off) OVER(ORDER BY monthh) as roll
+FROM rolling_total;
+
+
+SELECT company,YEAR(`date`),SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH company_year(company,years,total_laid_off) AS 
+(SELECT company,YEAR(`date`),SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+), company_year_rank AS
+(SELECT *, 
+DENSE_RANK() OVER(PARTITION BY years ORDER BY total_laid_off DESC ) AS ranking
+FROM company_year
+WHERE years is not null
+)
+SELECT * 
+FROM company_year_rank
+WHERE ranking<=5;
